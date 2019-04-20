faf0d964bb4cf0ed40cc286ca83fe8ca50148bb1
Annie Huang <annie.hn@gmail.com>
2019-04-20 17:47:44 +1000

    Add Message logic, mirror alert in bootstrap. And display message as long as message is available.

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import axios from 'axios';
+import Message from "./Message";
 
 const FileUpload = () => {
     const [file, setFile] = useState('');
@@ -32,6 +33,7 @@ const FileUpload = () => {
 
             const {fileName, filePath} = res.data;
             setUploadedFile({fileName, filePath});
+            setMessage('File Uploaded');
 
         } catch(err) {
 
@@ -46,6 +48,7 @@ const FileUpload = () => {
 
     return (
         <Fragment>
+            {message ? <Message msg={message}/> : null}
             <form onSubmit={onSubmit}>
                 <div className="custom-file mb-4">
                     <input
