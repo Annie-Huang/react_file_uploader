a84f310333c1ee2a59e167d484a01f02ca36ca9a
Annie Huang <annie.hn@gmail.com>
2019-04-20 16:31:39 +1000

    Make backend call and assign the return to upLoadedFile object.

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,20 +1,39 @@
 import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 const FileUpload = () => {
     const [file, setFile] = useState('');
     const [filename, setFilename] = useState('Choose File');
+
+    // What sends back from the backend will be an object with: fileName and fielPath (from server.js)
+    // res.json({fileName: file.name, filePath: `/uploads/${file.name}`});
+    const [uploadedFile, setUploadedFile] = useState({});
 
     const onChange = e => {
         setFile(e.target.files[0]);
         setFilename(e.target.files[0].name);
     };
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         const formData = new FormData();
 
         // Because we got this in server.js: const file = req.files.file;
         formData.append('file', file);
+
+        try {
+            const res = await axios.post('/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            const {fileName, filePath} = res.data;
+            setUploadedFile({fileName, filePath});
+
+        } catch(err) {
+
+        }
     };
 
     return (
