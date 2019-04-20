56afeb5a8490259d16d4c84183eeca28f53b91c9
Annie Huang <annie.hn@gmail.com>
2019-04-20 17:28:06 +1000

    Show the fileName and image after successful upload.

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -64,6 +64,14 @@ const FileUpload = () => {
                 />
             </form>
 
+            { uploadedFile ? (
+                <div className="row mt-5">
+                    <div className="col-md-6 m-auto">
+                        <h3 className="text-center">{ uploadedFile.fileName }</h3>
+                        <img style={{width: '100%'}} src={uploadedFile.filePath} alt="" />
+                    </div>
+                </div>
+            ): null }
         </Fragment>
     )
 };
