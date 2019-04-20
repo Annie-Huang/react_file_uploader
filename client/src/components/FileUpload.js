0d3ce7959cac46e3e1800877c20a131c6226fc88
Annie Huang <annie.hn@gmail.com>
2019-04-20 14:14:11 +1000

    Update App.js and apply react stateless component template to FileUpload.js

---
+++ client/src/components/FileUpload.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const FileUpload = () => {
+    return (
+        <Fragment>
+            
+        </Fragment>
+    );
+};
+
+export default FileUpload;
